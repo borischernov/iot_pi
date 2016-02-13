6047e6105b1856897a55c39dffe011475518a4df
borischernov <boris@imode.lv>
2016-02-13 19:06:17 +0200

    fix

--- lib/firmware_loader.rb
+++ lib/firmware_loader.rb
@@ -101,6 +101,12 @@ class FirmwareLoader
   
   def esptool(args)
     esptool = File.join(APP_ROOT, '/bin/esptool.py')
-    IO.popen("#{esptool} --port /dev/ttyAMA0 #{args}") { |io| status(io.gets) }
+    out = ""
+    IO.popen("#{esptool} --port /dev/ttyAMA0 #{args}") do |io| 
+      line = io.gets
+      out += line
+      status(line) 
+    end
+    out
   end
 end
