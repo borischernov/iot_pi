8b6ffd7ac5308d74244d3e9f379cbc6fab758b9c
borischernov <boris@imode.lv>
2016-02-16 00:42:05 +0200

    fix

--- controllers/api.rb
+++ controllers/api.rb
@@ -7,7 +7,9 @@
  end
 
 def set_data
-  sensor = Sensor.where(ident: params[:sensor]).first_or_create
+  sensor = Sensor.where(ident: params[:sensor]).first_or_create do |s|
+    s.sensor_type = "Temperature"
+  end
   sensor.update_attributes(last_seen_at: Time.now, address: request.ip)
   sensor.sensor_readings.create(value: params[:value], timestamp: params[:ts] ? Time.at(params[:ts]) : Time.now)
   "OK"
