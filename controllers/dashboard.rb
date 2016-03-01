ba115fa9d8b3a7c0bd400f5efa46e588ef06bc89
borischernov <boris@imode.lv>
2016-03-01 18:21:49 +0200

    order sensors by name

--- controllers/dashboard.rb
+++ controllers/dashboard.rb
@@ -1,10 +1,10 @@
 get '/' do
-  @sensors = Sensor.all
+  @sensors = Sensor.all.order(:name)
   erb :'dashboard/index'
 end
 
 get '/screen' do
-  @sensors = Sensor.all
+  @sensors = Sensor.all.order(:name)
   
   `/sbin/ifconfig eth0` =~ /inet\ addr\:(.*?)\ /
   @ip = $1.to_s.strip
