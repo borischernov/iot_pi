e69f163a5b3bf9268569db6e488f90ff8fde6dd2
borischernov <boris@imode.lv>
2016-05-15 01:01:22 +0300

    ADS1015 firmware for ESP8266

--- firmware/sensors/firmware.rb
+++ firmware/sensors/firmware.rb
@@ -5,6 +5,7 @@
     {name: 'sensor_type', title: 'Sensor Type', type: :select, options: [
       ['ds18b20', 'DS18B20 Temperature'],
       ['am2320', 'AM2320 Temperature / Relative Humidity'],
+      ['ads1015', 'ADS1015 4-channel ADC']
     ]},
     {name: 'deep_sleep', title: 'Use Deep Sleep', type: :checkbox},
     {name: 'poll_interval', title: 'Poll interval, sec.', type: :string, value: 60},
