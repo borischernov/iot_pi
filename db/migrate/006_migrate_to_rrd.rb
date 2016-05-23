e1f2a5b917f27eed1dcf02c83b46f9a2f2eae369
borischernov <boris@imode.lv>
2016-05-23 03:20:40 +0300

    execute vacuum outside of transaction

--- db/migrate/006_migrate_to_rrd.rb
+++ db/migrate/006_migrate_to_rrd.rb
@@ -2,6 +2,9 @@ class MigrateToRrd < ActiveRecord::Migration
   def up
     Sensor.all.each(&:create_rrd)
     drop_table :sensor_readings
-    ActiveRecord::Base.connection.execute("vacuum")
+    # Hack to execute vacuum outside of transaction
+    execute "commit"
+    execute "vacuum"
+    execute "begin transaction"
   end
 end
